9718d030709aaff07567efca447cd3dbb3c1b084
minwoo129 <key0129mw@naver.com>
2024-02-24 17:23:05 +0900

    env 파일 연결 기초 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: './src/config/.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
+  };
 };
