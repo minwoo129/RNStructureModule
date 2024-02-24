089c39db4f6ecb40c220ce0c62e22c674b28d6dd
minwoo129 <key0129mw@naver.com>
2024-02-24 17:08:35 +0900

    config 파일 생성

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+export const API_INFO = {
+  /** 내부 API 호출 공통경로 */
+  API_PATH: 'API 공통 경로',
+  /**
+   * 서버 타입
+   * - development: 개발서버
+   * - staging: 스테이징서버
+   * - production: 운영서버
+   */
+  SERVERTYPE: 'development',
+};
